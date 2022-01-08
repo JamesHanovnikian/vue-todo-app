e6ccf5efcefd1af3e9623e4864d462c9ec1366d1
YOUR NAME <jhanov@gmail.com>
2022-01-08 09:00:17 -0600

    adds add and delete buttons/functions

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <h1> VUE TODO! </h1>
+        <input class="input" v-model="newToDo "type="text" name="" id="">
+        <button v-on:click="add()" class="add-button"> Add </button>
+        <ul style="list-style-type:none"> 
+          <li v-for="(todo, i) in existingToDo">
+            <span> {{ i + 1 }} : {{ todo.text}} </span>
+            <button class="delete-button" v-on:click="delete()"> X </button>
+          </li>
+        </ul>
+      
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      newToDo: "",
+      existingToDo: [
+        { text: "Deliberate Practice work", id: 0 },
+        { text: "Apply to Jobs!", id: 1 },
+        { text: "CS50 Lecture 2", id: 2 },
+      ],
+    };
+  },
+  methods: {
+    add() {
+      this.existingToDo.push({ text: this.newToDo, id: new Date().valueOf() });
+      this.newToDo = "";
+    },
+    delete() {
+      this.existingToDo.splice(i);
+    },
+  },
+};
+</script>
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
