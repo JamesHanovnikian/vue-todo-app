246cf5a3662df49673f8cc910d7bb97d7c1274e9
YOUR NAME <jhanov@gmail.com>
2022-01-08 19:20:46 -0600

    add some styling to lis

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
+    
     <div class="container">
       <h1> VUE TODO! </h1>
-        <input class="input" v-model="newToDo "type="text" name="" id="">
-        <button v-on:click="add()" class="add-button"> Add </button>
-        <ul style="list-style-type:none"> 
-          <li v-for="(todo, i) in existingToDo">
-            <span> {{ i + 1 }} : {{ todo.text}} </span>
-            <button v-on:click="deleteToDo(i)" class="delete-button"> X </button>
-          </li>
-        </ul>
+      <div class="card" style="width: 75%;">
+        <input placeholder="Add your task" class="input" v-model="newToDo "type="text" name="" id="">
+        <div>
+          <button type="button" v-on:click="add()" class="btn btn-primary"> Add </button>
+        </div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item" v-for="(todo, i) in existingToDo">
+           <span> {{ i + 1 }} : {{ todo.text}} </span>
+            <button type="button" v-on:click="deleteToDo(i)" class="btn btn-danger"> <i class="fas fa-trash-alt"></i> </button>
+        </li>
+         </ul>
+      </div>
       
     </div>
   </div>
@@ -30,8 +35,14 @@ export default {
   },
   methods: {
     add() {
-      this.existingToDo.push({ text: this.newToDo, id: new Date().valueOf() });
-      this.newToDo = "";
+      if (this.newToDo === "") {
+      } else {
+        this.existingToDo.push({
+          text: this.newToDo,
+          id: new Date().valueOf(),
+        });
+        this.newToDo = "";
+      }
     },
     deleteToDo(i) {
       this.existingToDo.splice(i, 1);
@@ -44,4 +55,25 @@ export default {
 
 
 <style>
+body {
+  font-family: "Moon Dance", cursive;
+  font-size: 4rem;
+}
+
+h1 {
+  text-align: center;
+}
+
+.btn {
+  font-size: 3rem;
+}
+
+.card {
+  padding-top: 75px;
+  background-color: #3f3c3c;
+}
+
+.card .input {
+  width: 75%;
+}
 </style>
